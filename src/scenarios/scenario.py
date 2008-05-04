3eb63b2a96156f9d2bf9d4000cfd33fe4115f1b1
Alexis Laferrière <alexis.laf@xymus.net>
2008-05-04 01:58:57 +0000

    Actually added scenarios.

---
+++ src/scenarios/scenario.py
@@ -0,0 +1,12 @@
+class Scenario:
+    def __init__(self, game):
+        pass
+
+    def addRandomNpc( self, game, race=None, loc=None ):
+        pass
+
+    def doTurn( self, game ):
+        pass
+
+    def addShipForPlayer( self, game, player, shipId ):
+        pass
