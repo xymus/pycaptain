504f0714a34bb4791add262eeb0e75cfbda568e1
Alexis Laferrière <alexis.laf@xymus.net>
2010-04-05 17:59:00 +0000

    Makes missiles visibles when qty is over 0.

--- src/server/converters/local.py
+++ src/server/converters/local.py
@@ -164,7 +164,7 @@ class LocalConverter( Converter ):
                 else:
                     buildPerc = -1
 
-                missiles.append( COShips( missile,  missile != ids.M_NORMAL and missile != ids.M_AI and missile != ids.M_LARVA and missile != ids.M_EVOLVED and hasTurret, player.flagship.missiles[missile].amount>0, player.flagship.missiles[missile].amount, buildPerc, hasTurret and player.flagship.canBuild( game, missile ), hasTurret ) ) # TODO Remove hack to identify "usable missile"
+                missiles.append( COShips( missile,  missile != ids.M_NORMAL and missile != ids.M_AI and missile != ids.M_LARVA and missile != ids.M_EVOLVED and hasTurret, player.flagship.missiles[missile].amount>0, player.flagship.missiles[missile].amount, buildPerc, hasTurret and player.flagship.canBuild( game, missile ), hasTurret or player.flagship.missiles[missile].amount > 0 ) ) # TODO Remove hack to identify "usable missile"
                 missilesSpace = missilesSpace + player.flagship.missiles[missile].amount*game.stats[ missile ].hangarSpaceNeed
 
             t6 = time()
