f709ffb3ec4de82b0c589a1ac7872f448cdd22ed
Alexis Laferrière <alexis.laf@xymus.net>
2014-08-15 15:03:04 -0400

    server: ignore duplicated giveShip commands
    Signed-off-by: Alexis Laferrière <alexis.laf@xymus.net>

--- src/server/game.py
+++ src/server/game.py
@@ -153,7 +153,7 @@ class Game:
         if not player.flagship and player.points >= self.stats.PlayableShips[ shipId ].points:
             self.scenario.spawn( self, player, shipId )
         else:
-            raise Warning( "giveShip aborted, already has ship (%s) or selected ship unplayable (%s)." % (player.flagship, player.points < self.stats.PlayableShips[ shipId ].points)  )
+            print "giveShip ignored, already has ship (%s) or selected ship unplayable (%s)." % (player.flagship, player.points < self.stats.PlayableShips[ shipId ].points)
 
     def addPlayer( self, player ):
         self.relations[ player ] = {}
